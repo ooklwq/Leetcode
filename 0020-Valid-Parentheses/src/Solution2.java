91976b67ada47f25451d00dff54b8e41fa9e8d5a
ooklwq <ooklwq@163.com>
2020-03-26 20:16:33 +0800

    20 括号匹配简洁方法

---
+++ 0020-Valid-Parentheses/src/Solution2.java
@@ -0,0 +1,29 @@
+import java.util.Stack;
+
+class Solution2 {
+    public boolean isValid(String s) {
+        Stack<Character>  stack = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (c == '('){
+                stack.push(')');
+            }else if(c == '{'){
+                stack.push('}');
+            }else if(c == '['){
+                stack.push(']');
+            }else if( stack.isEmpty() || stack.pop()!=c){
+                return false;
+            }
+        }
+        return stack.isEmpty();
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new Solution2().isValid("()"));
+        System.out.println(new Solution2().isValid("()[]{}"));
+        System.out.println(new Solution2().isValid("([)]"));
+        System.out.println(new Solution2().isValid("(]"));
+        System.out.println(new Solution2().isValid("{[]}"));
+    }
+
+}
