e76e9aabb786d39f58797416ef4152d9ac14e1c7
ooklwq <ooklwq@163.com>
2020-03-28 23:03:14 +0800

    283 Move Zeroes finished

--- 0283-Move-Zeroes/src/Solution2.java
+++ 0283-Move-Zeroes/src/Solution2.java
@@ -1,21 +1,21 @@
-class Solution1 {
+class Solution2 {
     //Time Complexity: O(n)
     //Space Complexity: O(1)
     public void moveZeroes(int[] nums) {
         int pos = 0;
         for (int i = 0; i < nums.length; i++) {
             if (nums[i] != 0) {
-                nums[pos++] = nums[i];
+                int temp = nums[pos];
+                nums[pos] = nums[i];
+                nums[i] = temp;
+                pos++;
             }
-        }
-        for (int i = pos; i < nums.length; i++) {
-            nums[i] = 0;
         }
     }
 
     public static void main(String[] args) {
         int[] nums = new int[]{0, 1, 0, 3, 12};
-        new Solution1().moveZeroes(nums);
+        new Solution2().moveZeroes(nums);
         for (int i = 0; i < nums.length; i++) {
             System.out.println(nums[i]);
         }//[1,3,12,0,0]
