dc9f5d0644ad1805f6c4aeb5f8da4c9398aaa497
ooklwq <ooklwq@163.com>
2020-03-29 19:38:18 +0800

    876 Middle of the Linked List finished.

--- 0876-Middle-of-the-Linked-List/src/Solution1.java
+++ 0876-Middle-of-the-Linked-List/src/Solution1.java
@@ -12,23 +12,12 @@ class Solution1 {
         return data[size/2];
     }
 
-    private static ListNode initLinkedList(int[] nums){
-        //尾插法建立链表
-        ListNode head = new ListNode(nums[0]);
-        ListNode tail = head;
-        for (int i = 1; i < nums.length; i++) {
-            ListNode node = new ListNode(nums[i]);
-            tail.next = node;
-            tail = node;
-        }
-        return head;
-    }
     public static void main(String[] args) {
         int[] nums1 = new int[]{1,2,3,4,5};
-        ListNode linkedList1 = initLinkedList(nums1);
+        ListNode linkedList1 = new ListNode(nums1);
         System.out.println(new Solution1().middleNode(linkedList1).val);//3
         int[] nums2 = new int[]{1,2,3,4,5,6};
-        ListNode linkedList2 = initLinkedList(nums2);
+        ListNode linkedList2 = new ListNode(nums2);
         System.out.println(new Solution1().middleNode(linkedList2).val);//4
     }
 }
