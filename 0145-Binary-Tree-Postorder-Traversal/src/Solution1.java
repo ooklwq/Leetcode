0b359676af38ce52502b06e71b4999315d16d980
ooklwq <ooklwq@163.com>
2020-04-04 13:29:39 +0800

    增加145 二叉树非递归后续遍历

--- 0145-Binary-Tree-Postorder-Traversal/src/Solution1.java
+++ 0145-Binary-Tree-Postorder-Traversal/src/Solution1.java
@@ -1,18 +1,24 @@
-import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Stack;
 
 class Solution1 {
+    //Time Complexity: O(n)
+    //Space Complexity： O(h)
+    //用栈模拟递归
+    //1.访问根节点；2.访问右子树；3.访问左子树；4.逆转（采用链表头插法避免逆转操作）
     public List<Integer> postorderTraversal(TreeNode root) {
-        List<Integer> res = new ArrayList<>();
-        postorderTraversal(root, res);
+        List<Integer> res = new LinkedList<>();
+        if (root == null) return res;
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+        while (!stack.isEmpty()){
+            TreeNode cur = stack.pop();
+            res.add(0, cur.val);
+            if (cur.left != null) stack.push(cur.left);
+            if (cur.right != null) stack.push(cur.right);
+        }
         return res;
     }
 
-    private List<Integer> postorderTraversal(TreeNode root, List<Integer> list){
-        if ( root == null) return list;
-        postorderTraversal(root.left, list);
-        postorderTraversal(root.right, list);
-        list.add(root.val);
-        return list;
-    }
 }
