c387273efffedfbc0f8782ac23deee50a9c9bce2
ooklwq <ooklwq@163.com>
2020-05-07 18:14:25 +0800

    226 iterative BFS and DFS

--- 0226-Invert-Binary-Tree/src/Solution1.java
+++ 0226-Invert-Binary-Tree/src/Solution1.java
@@ -1,2 +1,22 @@
-public class Solution1 {
+import java.util.LinkedList;
+import java.util.Queue;
+
+class Solution1 {
+
+    //Time Complexity: O(n)
+    //Space Complexity: O(n)
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) return root;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()){
+            TreeNode cur = queue.remove();
+            TreeNode temp = cur.right;
+            cur.right = cur.left;
+            cur.left = temp;
+            if (cur.left != null) queue.add(cur.left);
+            if (cur.right != null) queue.add(cur.right);
+        }
+        return root;
+    }
 }
--- 0226-Invert-Binary-Tree/src/Solution2.java
+++ 0226-Invert-Binary-Tree/src/Solution2.java
@@ -1,4 +1,21 @@
-package PACKAGE_NAME;
+import java.util.Stack;
 
-public class Solution2 {
+class Solution2 {
+
+    //Time Complexity: O(n)
+    //Space Complexity: O(h)
+    public TreeNode invertTree(TreeNode root) {
+        if (root == null) return root;
+        Stack<TreeNode> stack = new Stack<>();
+        stack.push(root);
+        while (!stack.isEmpty()){
+            TreeNode cur = stack.pop();
+            TreeNode temp = cur.right;
+            cur.right = cur.left;
+            cur.left = temp;
+            if (cur.left != null) stack.push(cur.left);
+            if (cur.right != null) stack.push(cur.right);
+        }
+        return root;
+    }
 }
