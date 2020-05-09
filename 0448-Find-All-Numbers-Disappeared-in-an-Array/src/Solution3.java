4c7ce03f4f9473a399011340f83a5fdb29ba4a6e
ooklwq <ooklwq@163.com>
2020-05-09 11:36:53 +0800

    448 modify Solution3

--- 0448-Find-All-Numbers-Disappeared-in-an-Array/src/Solution3.java
+++ 0448-Find-All-Numbers-Disappeared-in-an-Array/src/Solution3.java
@@ -4,6 +4,7 @@ import java.util.List;
 class Solution3 {
 
     //Time Complexity: O(n)
+    //在第二层循环中总共会交换n次，均摊到每一次循环为O(1)
     //Space Complexity: O(1)
     public List<Integer> findDisappearedNumbers(int[] nums) {
         List<Integer> res = new ArrayList<>();
@@ -21,6 +22,7 @@ class Solution3 {
                 nums[nums[i] - 1] = nums[i];
                 nums[i] = temp;
             }
+
         }
         for (int i = 0; i < nums.length; i++) {
             if (nums[i] != i + 1){
