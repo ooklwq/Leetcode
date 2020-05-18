61b71e41667dee8b8462362895f7115648d07daa
ooklwq <ooklwq@163.com>
2020-05-18 22:23:21 +0800

    215 QuickSelect O(n)

--- 0215-Kth-Largest-Element-in-an-Array/src/Solution2.java
+++ 0215-Kth-Largest-Element-in-an-Array/src/Solution2.java
@@ -1,12 +1,77 @@
-import java.util.PriorityQueue;
+import java.util.Random;
 
 class Solution2 {
 
     //Time Complexity: O(n)
-    //Space Complexity: O(k)
+    //Space Complexity: O(1)
     //Quick Select
     public int findKthLargest(int[] nums, int k) {
-
+        int n = nums.length;
+        int res = nums.length - k;
+        int l = 0, r = n - 1;
+        while (true){
+            int pos = partition(nums, l, r);
+            if (pos == res){
+                return nums[pos];
+            }else if (pos < res){
+                l = pos + 1;
+            }else {
+                r = pos - 1;
+            }
+        }
     }
 
+    private int partition(int[] nums, int l, int r){
+        //随机选择枢纽元素
+        int randomIndex = new Random().nextInt(r-l+1) + l;
+        swap(nums, l, randomIndex);
+        int pivot = nums[l];
+        //nums[l+1, j] < nums[l]
+        //nums[j+1, i-1] > nums[l]
+        int j = l;
+        for (int i = l + 1; i <=r ; i++) {
+            if (nums[i] < pivot){
+                swap(nums, ++j, i);
+            }
+        }
+        swap(nums, l, j);
+        return j;
+    }
+
+    //双路
+    private int partition2(int[] nums, int l, int r){
+        //随机选择枢纽元素
+        int randomIndex = new Random().nextInt(r-l+1) + l;
+        swap(nums, l, randomIndex);
+        int pivot = nums[l];
+        int i = l + 1, j = r;
+        //[l+1, i-1] <= pivot
+        //[j+1, r] >= pivot
+        while (i <= j){
+            while (i <= j && nums[i] < pivot) i++;
+            while (i <= j && nums[j] > pivot) j--;
+            if (i > j) break;
+            swap(nums, i++, j--);
+        }
+//        while (i <= j){
+//            if (nums[i] > pivot && nums[j] < pivot){
+//                swap(nums, i++, j--);
+//            }
+//            if (nums[i] <= pivot) i++;
+//            if (nums[j] >= pivot) j--;
+//        }
+        swap(nums, l, j);
+        return j;
+    }
+
+    private void swap(int[] nums, int i, int j){
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {3, 2, 1, 5, 6, 4};
+        System.out.println(new Solution2().findKthLargest(nums, 2));
+    }
 }
