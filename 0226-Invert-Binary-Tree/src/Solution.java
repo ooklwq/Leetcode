122ab089bc10d240701e994dc2369614d218551e
ooklwq <ooklwq@163.com>
2020-11-27 15:31:21 +0800

    226 solved.

--- 0226-Invert-Binary-Tree/src/Solution.java
+++ 0226-Invert-Binary-Tree/src/Solution.java
@@ -4,9 +4,14 @@ class Solution {
     //Space Complexity: O(h)
     public TreeNode invertTree(TreeNode root) {
         if (root == null) return null;
-        TreeNode prevRight = root.right;
-        root.right = invertTree(root.left);
-        root.left = invertTree(prevRight);
+//        TreeNode prevRight = root.right;
+//        root.right = invertTree(root.left);
+//        root.left = invertTree(prevRight);
+        invertTree(root.left);
+        invertTree(root.right);
+        TreeNode temp = root.right;
+        root.right = root.left;
+        root.left = temp;
         return root;
     }
 }
