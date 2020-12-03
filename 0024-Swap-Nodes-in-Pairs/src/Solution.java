42e63e569afc82fec7854960078195d2fc72fa27
ooklwq <ooklwq@163.com>
2020-12-03 10:05:49 +0800

    24 solved.

---
+++ 0024-Swap-Nodes-in-Pairs/src/Solution.java
@@ -0,0 +1,14 @@
+class Solution {
+
+    //Time Complexity:O(n)
+    //Space Complexity:O(n)
+    public ListNode swapPairs(ListNode head) {
+        if (head == null || head.next == null) return head;
+        ListNode next = head.next;
+        head.next = swapPairs(next.next);
+        next.next = head;
+        return next;
+    }
+
+
+}
