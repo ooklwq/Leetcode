78424bbf7604cea3005caf2701f01ebeab36da1c
ooklwq <ooklwq@163.com>
2020-12-25 10:38:59 +0800

    322 518 solved.

--- 0322-Coin-Change/src/Solution.java
+++ 0322-Coin-Change/src/Solution.java
@@ -12,10 +12,13 @@ class Solution {
         Arrays.fill(dp, amount + 1);
         dp[0] = 0;
         for (int i = 0; i < coins.length; i++) {
-            for (int j = 0; j <= amount; j++) {
-                if (j >= coins[i]){
+//            for (int j = 0; j <= amount; j++) {
+//                if (j >= coins[i]){
+//                    dp[j] = Math.min(dp[j], dp[j- coins[i]] + 1);
+//                }
+//            }
+            for (int j = coins[i]; j <= amount; j++) {
                     dp[j] = Math.min(dp[j], dp[j- coins[i]] + 1);
-                }
             }
         }
         return dp[amount] > amount ? -1 : dp[amount];
--- 0518-Coin-Change-2/src/Solution.java
+++ 0518-Coin-Change-2/src/Solution.java
@@ -1,8 +1,18 @@
 class Solution {
 
-    //Time Complexity:
-    //Space Complexity:
+    //Time Complexity: O(n*C)
+    //Space Complexity: O(C)
     public int change(int amount, int[] coins) {
-
+        if (coins == null || amount < 0) {
+            return 0;
+        }
+        int[] dp = new int[amount + 1];
+        dp[0] = 1;
+        for (int coin : coins) {
+            for (int i = coin; i <= amount; i++) {
+                dp[i] += dp[i - coin];
+            }
+        }
+        return dp[amount];
     }
 }
