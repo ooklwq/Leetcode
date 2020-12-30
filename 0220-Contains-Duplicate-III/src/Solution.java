a69ce34285467831b96dd4da7fe996ce5b0396de
ooklwq <ooklwq@163.com>
2020-12-30 19:49:44 +0800

    220 solved.

--- 0220-Contains-Duplicate-III/src/Solution.java
+++ 0220-Contains-Duplicate-III/src/Solution.java
@@ -5,20 +5,20 @@ class Solution {
     //Time Complexity: O(nlogk)
     //Space Complexity: O(k)
     public boolean containsNearbyAlmostDuplicate(int[] nums, int k, int t) {
-        if (nums == null || nums.length == 0 || k <= 0) {
+        if (nums == null || nums.length == 0 || k <= 0 || t < 0) {
             return false;
         }
         TreeSet<Long> set = new TreeSet<>();
         for (int i = 0; i < nums.length; i++) {
-            Long ceil = set.ceiling(new Long(nums[i]) - new Long(t));
-            if (ceil != null && ceil <= new Long(nums[i]) + new Long(t)) {
+            Long ceil = set.ceiling((long)nums[i] - t);
+            if (ceil != null && ceil <= (long)nums[i] + t) {
                 return true;
             } else {
-                set.add(new Long(nums[i]));
+                set.add((long)nums[i]);
             }
             //维持set的大小为k
             if (set.size() == k + 1) {
-                set.remove(new Long(nums[i - k]));
+                set.remove((long)nums[i - k]);
             }
         }
         return false;
