c66e21db0abb3f049f5ed7ca566f0982b97d87a3
ooklwq <ooklwq@163.com>
2021-01-08 08:56:26 +0800

    153 154 solved.

---
+++ 0153-Find-Minimum-in-Rotated-Sorted-Array/src/Solution.java
@@ -0,0 +1,20 @@
+class Solution {
+
+    //Time Complexity: O(logn)
+    //Space Complexity: O(1)
+    public int findMin(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        int left = 0, right = nums.length - 1;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] < nums[right]) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return nums[left];
+    }
+}
---
+++ 0154-Find-Minimum-in-Rotated-Sorted-Array-II/src/Solution.java
@@ -0,0 +1,22 @@
+class Solution {
+
+    //Time Complexity: O(logn)
+    //Space Complexity: O(1)
+    public int findMin(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        int left = 0, right = nums.length - 1;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] < nums[right]) {
+                right = mid;
+            } else if (nums[mid] > nums[right]) {
+                left = mid + 1;
+            } else {
+                right --;
+            }
+        }
+        return nums[left];
+    }
+}
