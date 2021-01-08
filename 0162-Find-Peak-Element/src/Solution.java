cfed4dfd9dd377aae22f66cae04d3d399b934b53
ooklwq <ooklwq@163.com>
2021-01-08 09:30:51 +0800

    162 solved.

---
+++ 0162-Find-Peak-Element/src/Solution.java
@@ -0,0 +1,20 @@
+class Solution {
+
+    //Time Complexity: O(logn)
+    //Space Complexity: O(1)
+    public int findPeakElement(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return -1;
+        }
+        int left = 0, right = nums.length - 1;
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] < nums[mid + 1]) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        return left;
+    }
+}
