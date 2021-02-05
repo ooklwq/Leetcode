d066519684ce24f52e0099c5a630576d87c39c8d
ooklwq <ooklwq@163.com>
2021-02-05 11:12:15 +0800

    701 solved.

---
+++ 0701-Insert-into-a-Binary-Search-Tree/src/Solution.java
@@ -0,0 +1,16 @@
+class Solution {
+
+    //Time Complexity: O(logn)
+    //Space Complexity: O(1)
+    public TreeNode insertIntoBST(TreeNode root, int val) {
+        if (root == null) {
+            return new TreeNode(val);
+        }
+        if (root.val > val) {
+            root.left =  insertIntoBST(root.left, val);
+        } else if (root.val < val) {
+            root.right = insertIntoBST(root.right, val);
+        }
+        return root;
+    }
+}
