02fb2d42e1ef9868de7ac15bcacf41dadfabdc4c
ooklwq <ooklwq@163.com>
2021-03-12 08:34:23 +0800

    242 solved.

--- 0242-Valid-Anagram/src/Solution2.java
+++ 0242-Valid-Anagram/src/Solution2.java
@@ -3,18 +3,13 @@ class Solution2 {
     //Time Complexity: O(n)
     //Space Complexity: O(1)
     public boolean isAnagram(String s, String t) {
-        if (s.length() != t.length()) return false;
         int[] alphabet = new int[26];
-        for (int i = 0; i < s.length(); i++) {
-            alphabet[s.charAt(i) - 'a'] ++;
-        }
+        for (int i = 0; i < s.length(); i++) alphabet[s.charAt(i) - 'a']++;
         for (int i = 0; i < t.length(); i++) {
-            alphabet[t.charAt(i) - 'a'] --;
-            if (alphabet[t.charAt(i) - 'a'] < 0) return false;
+            alphabet[t.charAt(i) - 'a']--;
+            if(alphabet[t.charAt(i) - 'a'] < 0) return false;
         }
-//        for (int alpha : alphabet) {
-//            if (alpha != 0) return false;
-//        }
+        for (int i : alphabet) if (i != 0) return false;
         return true;
     }
 
