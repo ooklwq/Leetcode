3f372a9454160ef68010729e380030b7d8077bdd
ooklwq <ooklwq@163.com>
2021-05-15 21:27:50 +0800

    86 solved.

--- 0086-Partition-List/src/Solution.java
+++ 0086-Partition-List/src/Solution.java
@@ -1,8 +1,25 @@
+
+
 class Solution {
 
-    //Time Complexity:
-    //Space Complexity:
+    //Time Complexity: O(n)
+    //Space Complexity: O(1)
     public ListNode partition(ListNode head, int x) {
-
+        ListNode dummySmall = new ListNode(-1);
+        ListNode dummyBig = new ListNode(-1);
+        ListNode small = dummySmall, big = dummyBig;
+        while (head != null) {
+            if (head.val < x) {
+                small.next = head;
+                small = small.next;
+            } else {
+                big.next = head;
+                big = big.next;
+            }
+            head = head.next;
+        }
+        big.next = null;
+        small.next = dummyBig.next;
+        return dummySmall.next;
     }
 }
